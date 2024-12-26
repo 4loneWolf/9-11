b57060d6e152e918ec3bb3c74c1b93a5ea40b627
pepega <drelkasteam@gmail.com>
2024-12-26 21:05:24 +0700

    Третья итерация (III) - Функция выбора генерации/ввода массива

--- main.py
+++ main.py
@@ -21,8 +21,21 @@ def input_array(bool=False):
 
     return array
 
+def create_array(bool=False):
+    choice = input("Хотите ввести массив вручную (m) или сгенерировать случайным образом (r)? ")
+    if choice.lower() == 'm':
+        return input_array(bool)
+    elif choice.lower() == 'r':
+        size = int(input("Введите размер массива: "))
+        min_value = int(input("Введите минимальное значение: "))
+        max_value = int(input("Введите максимальное значение: "))
+        return generate_array(size, min_value, max_value, bool)
+    else:
+        print("Неверный выбор. Попробуйте снова.")
+        return create_array(bool)
+
 def main():
-    print(input_array())
-    print(input_array(True))
+    print(create_array())
+    print(create_array(True))
 
 main()
