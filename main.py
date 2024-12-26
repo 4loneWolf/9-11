443aafbe06c014670bf162a5064730ab26b29955
pepega <drelkasteam@gmail.com>
2024-12-26 21:03:26 +0700

    Вторая итерация (II) - Функция ручного ввода  массива

--- main.py
+++ main.py
@@ -6,8 +6,23 @@ def generate_array(size, min_value, max_value, bool=False):
     else:
         return [random.randint(min_value, max_value) for _ in range(size)]
 
+def input_array(bool=False):
+    array = []
+    if bool:
+        rows = int(input("Введите количество подмассивов: "))
+        columns = int(input("Введите количество элементов массива: "))
+        for a in range(rows):
+            for b in range(columns):
+                array.append(int(input(f"Введите элемент массива [{a}][{b}]: ")))
+    else:
+        size = int(input("Введите размер массива: "))
+        for _ in range(size):
+            array.append(int(input("Введите элемент массива: ")))
+
+    return array
+
 def main():
-    print(generate_array(5, 0, 10))
-    print(generate_array(5, 0, 10, True))
+    print(input_array())
+    print(input_array(True))
 
 main()
