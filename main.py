5cbd7dc566a59eaa5769ad18233c0e294afdf8c6
pepega <drelkasteam@gmail.com>
2024-12-26 21:40:39 +0700

    Восьмая итерация (VIII) - Добавлена документация

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import random
 
 def generate_array(size, min_value, max_value, bool=False):
+    """
+    Генерирует массив случайных чисел.
+
+    :size: Размер массива.
+    :min_value: Минимальное значение элементов массива.
+    :max_value: Максимальное значение элементов массива.
+    :bool: Флаг для генерации массива подмассивов.
+    :return: Сгенерированный массив.
+    """
     if bool:
         return [[random.randint(min_value, max_value) for _ in range(size)] for _ in range(size)]
     else:
         return [random.randint(min_value, max_value) for _ in range(size)]
 
 def input_array(bool=False):
+    """
+    Вводит массив с клавиатуры.
+
+    :param bool: Флаг для ввода массива подмассивов.
+    :return: Введенный массив.
+    """
     array = []
     if bool:
         rows = int(input("Введите количество подмассивов: "))
@@ -22,6 +37,12 @@ def input_array(bool=False):
     return array
 
 def create_array(bool=False):
+    """
+    Создает массив, выбирая между вводом с клавиатуры и генерацией случайным образом.
+
+    :param bool: Флаг для создания массива подмассивов.
+    :return: Созданный массив.
+    """
     choice = input("Хотите ввести массив вручную (m) или сгенерировать случайным образом (r)? ")
     if choice.lower() == 'm':
         return input_array(bool)
@@ -35,6 +56,13 @@ def create_array(bool=False):
         return create_array(bool)
 
 def count_subarrays_with_sum(array, target_sum):
+    """
+    Подсчитывает количество подмассивов, сумма элементов которых равна заданному числу.
+
+    :param array: Массив подмассивов.
+    :param target_sum: Заданное число для проверки суммы подмассивов.
+    :return: Количество подмассивов с заданной суммой.
+    """
     count = 0
     current_sum = 0
     start = 0
@@ -49,7 +77,21 @@ def count_subarrays_with_sum(array, target_sum):
     return count
 
 def count_common_elements(array1, array2):
+    """
+    Подсчитывает количество общих чисел в двух массивах, включая перевернутые версии.
+
+    :param array1: Первый массив.
+    :param array2: Второй массив.
+    :return: Количество общих чисел.
+    """
     def is_reversed(num1, num2):
+        """
+        Проверяет, является ли одно число перевернутой версией другого.
+
+        :param num1: Первое число.
+        :param num2: Второе число.
+        :return: True, если num1 является перевернутой версией num2, иначе False.
+        """
         return str(num1)[::-1] == str(num2)
 
     common_count = 0
@@ -61,7 +103,23 @@ def count_common_elements(array1, array2):
     return common_count
 
 def can_form_number(array1, array2, array3):
+    """
+    Проверяет, можно ли получить число из третьего массива арифметическими преобразованиями с числами из двух других массивов.
+
+    :param array1: Первый массив.
+    :param array2: Второй массив.
+    :param array3: Третий массив.
+    :return: Список результатов проверки.
+    """
     def can_form(num1, num2, target):
+        """
+        Проверяет, можно ли получить число target арифметическими преобразованиями с числами num1 и num2.
+
+        :param num1: Первое число.
+        :param num2: Второе число.
+        :param target: Целевое число.
+        :return: True, если можно получить число target, иначе False.
+        """
         return num1 + num2 == target or num1 - num2 == target or num2 - num1 == target or num1 * num2 == target or (num2 != 0 and num1 / num2 == target) or (num1 != 0 and num2 / num1 == target)
     print(array2)
     results = []
